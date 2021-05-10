9a163a03d6bdbdda15cf47169ac19accb45c9957
Eason Liu <bettercalleason@gmail.com>
2021-05-10 19:48:04 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,582 @@
+var DATA = {
+  type: 'SSR Generated Data',
+  previewMode: '',
+  draftMode: '',
+  domainName: 'watson.la',
+  home: [
+    {
+      id: 'home-2901',
+      type: 'work',
+      homeline: 'The Magic<br>of the Movies',
+      info: 'Mank',
+      client: 'Netflix',
+      column: 5,
+      media: {
+        kind: 'video',
+        width: 1860,
+        height: 1080,
+        loop: true,
+        cover:
+          'imager/cms/home/2860/MNK_TUMB_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        src: 'cms/home/MNK_TUMB.mp4',
+        palette: ['#1e1f1f'],
+      },
+      url: '/work/mank',
+    },
+
+    {
+      id: 'home-2230',
+      type: 'work',
+      homeline: 'Woodstock.<br>The moon landing.<br>Love. Art. War.',
+      info: 'Once Upon a Time in…',
+      client: 'Sony Entertainment',
+      column: 4,
+      media: {
+        kind: 'image',
+        width: 1925,
+        height: 1540,
+        src: 'imager/cms/home/2257/OUATIH_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        palette: ['#f4bc46'],
+      },
+      url: '/work/once-upon-a-time-in-hollywood',
+    },
+
+    {
+      id: 'home-2207',
+      type: 'work',
+      homeline: 'What&#8217;s in the<br>box?',
+      info: 'Mystery Box Live!',
+      client: 'The Black Tux',
+      column: 2,
+      media: {
+        kind: 'video',
+        width: 770,
+        height: 1080,
+        loop: true,
+        cover: 'imager/cms/home/1350/tux_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        src: 'cms/home/tux.mp4',
+        palette: ['#e5b6ff'],
+      },
+      url: '/work/the-black-tux',
+    },
+
+    {
+      id: 'home-2209',
+      type: 'work',
+      homeline: 'Suspiria',
+      info: 'Suspiria',
+      client: 'Amazon Studios',
+      column: 5,
+      media: {
+        kind: 'video',
+        width: 1750,
+        height: 1030,
+        loop: true,
+        cover:
+          'imager/cms/home/1360/suspiria_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        src: 'cms/home/suspiria.mp4',
+        palette: ['#755650'],
+      },
+      url: '/work/suspiria',
+    },
+
+    {
+      id: 'home-2208',
+      type: 'work',
+      homeline: 'Stream it if you<br>feel it',
+      info: 'FX Social',
+      client: 'FX',
+      column: 3,
+      media: {
+        kind: 'image',
+        width: 1360,
+        height: 1080,
+        src: 'imager/cms/home/1353/fx_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        palette: ['#414cda'],
+      },
+      url: '/work/fx',
+    },
+
+    {
+      id: 'home-2211',
+      type: 'work',
+      homeline: 'Get your paws<br>dirty',
+      info: 'Isle of Dogs',
+      client: 'Fox Searchlight',
+      column: 3,
+      media: {
+        kind: 'image',
+        width: 1200,
+        height: 1350,
+        src:
+          'imager/cms/home/1357/isle-of-dogs_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        palette: ['#ffcc5c'],
+      },
+      url: '/work/isle-of-dogs',
+    },
+
+    {
+      id: 'home-2210',
+      type: 'other',
+      homeline: 'Big Night! We won 8 Clios at 2018 Clio Entertainment Awards',
+      info: 'Nov 16',
+      client: null,
+      column: 4,
+      media: {
+        kind: 'image',
+        width: 1000,
+        height: 550,
+        src: 'imager/cms/home/1355/clio_e64401d78e4f5706cb1ea80fb92b7d7d.png',
+        palette: ['#d2aa40'],
+      },
+      url: '/news/clio-entertainment-2018',
+    },
+  ],
+  news: [
+    {
+      id: 'get-an-in-depth-look-at-our-once-upon-a-time-magazine',
+      title:
+        'Get\u0020an\u0020in\u002Ddepth\u0020look\u0020at\u0020our\u0020digital\u0020campaign\u0020for\u0020Once\u0020Upon\u0020A\u0020Time\u0020in\u0020Hollywood\u0020in\u0020Communication\u0020Arts\u0020Magazine',
+      subtitle: 'Aug 26',
+      url: 'https://www.commarts.com/webpicks/once-upon-a-time-in-hollywood',
+    },
+    {
+      id:
+        'if-youre-passionate-about-film-tv-and-have-experience-with-social-media-editing-youd-be-a-great-fit',
+      title:
+        'If\u0020you\u0027re\u0020passionate\u0020about\u0020film\u0020\u0026\u0020TV\u0020and\u0020have\u0020experience\u0020with\u0020social\u0020media\u0020video\u0020editing,\u0020you\u0027d\u0020be\u0020a\u0020great\u0020fit\u0021',
+      subtitle: 'Aug 13',
+      url: '',
+    },
+    {
+      id: 'quentin-tarantino-says-our-digital-magazine-is-required-reading',
+      title:
+        'Quentin\u0020Tarantino\u0020says\u0020our\u0020digital\u0020magazine\u0020is\u0020required\u0020reading\u0021',
+      subtitle: 'Aug 13',
+      url:
+        'https://www.esquire.com/uk/latest-news/a28562196/once-upon-a-time-in-hollywood-online-magazine/',
+    },
+    {
+      id: 'clio-entertainment-2018',
+      title:
+        'Big\u0020Night\u0021\u0020We\u0020won\u00208\u0020Clios\u0020at\u00202018\u0020Clio\u0020Entertainment\u0020Awards',
+      subtitle: 'Nov 16',
+      url: '/news/clio-entertainment-2018',
+    },
+    {
+      id: 'kikk-festival',
+      title:
+        'Hello,\u0020Belgium\u0021\u0020Watson\u0020lead\u0020CD\u0020Fernando\u0020Ramirez\u0020is\u0020heading\u0020to\u0020KiKK\u0020Festival\u0020Nov\u00202nd',
+      subtitle: 'Sep 14',
+      url: '/news/kikk-festival',
+    },
+  ],
+  join: [
+    {
+      id: 'senior-account-manager',
+      title: 'Senior\u0020Account\u0020Manager',
+      url: '/join/senior-account-manager',
+    },
+    {
+      id: 'acd',
+      title:
+        'Associative\u0020Creative\u0020Director\u0020\u0028Strategy/Writing\u0029',
+      url: '/join/acd',
+    },
+    {
+      id: 'strategist',
+      title: 'Strategist',
+      url: '/join/strategist',
+    },
+    {
+      id: 'creative-director',
+      title: 'Creative\u0020Director\u0020\u00D7\u0020Interactive',
+      url: '/join/creative-director',
+    },
+    {
+      id: 'motion-designer',
+      title: 'Motion\u0020Designer',
+      url: '/join/motion-designer',
+    },
+    {
+      id: 'interactive-art-director',
+      title: 'Art\u0020Director\u0020\u00D7\u0020Interactive',
+      url: '/join/interactive-art-director',
+    },
+    {
+      id: 'office-assistant',
+      title: 'Office\u0020Assistant',
+      url: '/join/office-assistant',
+    },
+  ],
+  work: [
+    {
+      id: 'mank',
+      title: 'Mank',
+      caseline: 'The Magic<br>of the Movies',
+      client: 'Netflix',
+      media: {
+        kind: 'image',
+        palette: ['#1e1f1f'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/2986/MANK_TH_FIX1_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/mank',
+    },
+    {
+      id: 'raised-by-wolves',
+      title: 'Raised By Wolves',
+      caseline: 'Welcome<br>to Kepler-22b',
+      client: 'HBO Max',
+      media: {
+        kind: 'image',
+        palette: ['#3333ff'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/2770/MNK_TH2_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/raised-by-wolves',
+    },
+    {
+      id: 'angel-of-darkness',
+      title: 'Angel of Darkness',
+      caseline: 'An unspeakable<br>crime, a mind<br>bending mystery.',
+      client: 'TNT',
+      media: {
+        kind: 'image',
+        palette: ['#85d6cb'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/2987/AOD_THUMB53_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/angel-of-darkness',
+    },
+    {
+      id: 'once-upon-a-time-in-hollywood',
+      title: 'Once Upon a Time in…',
+      caseline: 'Woodstock. <br>The moon landing. <br>Love. Art. War.',
+      client: 'Sony Entertainment',
+      media: {
+        kind: 'image',
+        palette: ['#f4bc46'],
+        width: 800,
+        height: 800,
+        src:
+          'imager/cms/work-landing/2158/ouatih_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/once-upon-a-time-in-hollywood',
+    },
+    {
+      id: 'dumbo',
+      title: 'Dumbo',
+      caseline: 'A place <br>where dreams <br>come true',
+      client: 'Disney',
+      media: {
+        kind: 'image',
+        palette: ['#7fc8d1'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/1894/Dumbo_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/dumbo',
+    },
+    {
+      id: 'fx',
+      title: 'FX Social',
+      caseline: 'What<br>does it mean<br>to be fearless?',
+      client: 'FX',
+      media: {
+        kind: 'image',
+        palette: ['#3333ff'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/237/fx_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/fx',
+    },
+    {
+      id: 'suspiria',
+      title: 'Suspiria',
+      caseline: 'Let mother<br>take care<br>of you',
+      client: 'Amazon Studios',
+      media: {
+        kind: 'image',
+        palette: ['#755650'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/250/suspiria_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/suspiria',
+    },
+    {
+      id: 'the-black-tux',
+      title: 'Mystery Box Live!',
+      caseline: 'What&#8217;s<br>in the box?',
+      client: 'The Black Tux',
+      media: {
+        kind: 'image',
+        palette: ['#e5b6ff'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/251/tbt_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/the-black-tux',
+    },
+    {
+      id: 'the-death-of-stalin',
+      title: 'The Death of Stalin',
+      caseline: 'The Presidium<br>approves this<br>message',
+      client: 'IFC Films',
+      media: {
+        kind: 'image',
+        palette: ['#be1529'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/249/stalin_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/the-death-of-stalin',
+    },
+    {
+      id: 'isle-of-dogs',
+      title: 'Isle of Dogs',
+      caseline: 'Get your<br>paws dirty',
+      client: 'Fox Searchlight',
+      media: {
+        kind: 'image',
+        palette: ['#ffcc5c'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/241/iod_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/isle-of-dogs',
+    },
+    {
+      id: 'the-killing-of-a-sacred-deer',
+      title: 'The Killing of a Sacred Deer',
+      caseline: 'You might<br>want to sit<br>down for this',
+      client: 'A24',
+      media: {
+        kind: 'image',
+        palette: ['#3e677e'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/243/ksd_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/the-killing-of-a-sacred-deer',
+    },
+    {
+      id: 'disaster-artist',
+      title: 'The Disaster Artist',
+      caseline: 'Be hero,<br>not villain',
+      client: 'A24',
+      media: {
+        kind: 'image',
+        palette: ['#6acaae'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/220/da_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/disaster-artist',
+    },
+    {
+      id: 'lady-bird',
+      title: 'Lady Bird',
+      caseline: 'Fly away<br>home',
+      client: 'A24',
+      media: {
+        kind: 'image',
+        palette: ['#c6a678'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/244/lb_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/lady-bird',
+    },
+    {
+      id: 'ghost-store',
+      title: 'A Ghost Store',
+      caseline: 'Are you<br>ready to<br>check out?',
+      client: 'A24',
+      media: {
+        kind: 'image',
+        palette: ['#424242'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/239/ghost_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/ghost-store',
+    },
+    {
+      id: 'moonlight',
+      title: 'Moonlight',
+      caseline: 'Digital word<br>of mouth',
+      client: 'A24',
+      media: {
+        kind: 'image',
+        palette: ['#03efe6'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/246/moonlight_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/moonlight',
+    },
+    {
+      id: 'sing',
+      title: 'Sing',
+      caseline: 'Become<br>who you were<br>meant to be',
+      client: 'Universal',
+      media: {
+        kind: 'image',
+        palette: ['#39967b'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/248/sing_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/sing',
+    },
+    {
+      id: 'the-hunger-games',
+      title: 'The Hunger Games',
+      caseline: 'Revolution<br>is in the air',
+      client: 'Lionsgate',
+      media: {
+        kind: 'image',
+        palette: ['#9da793'],
+        width: 600,
+        height: 600,
+        src:
+          'imager/cms/work-landing/240/hg_3d86fe779b9d937a164ee6b8559e030f.png',
+      },
+      url: '/work/the-hunger-games',
+    },
+  ],
+  about: {
+    visuals: [
+      { src: 'cms/visuals/ghost.png' },
+      { src: 'cms/visuals/fire.png' },
+      { src: 'cms/visuals/maria.png' },
+      { src: 'cms/visuals/bird.png' },
+      { src: 'cms/visuals/lady.png' },
+    ],
+    reel: {
+      media: [
+        {
+          width: 1920,
+          height: 1080,
+          loop: false,
+          triangle: 'black',
+          cover: 'cms/reel-cover.png',
+          src: 'cms/reel-2019.mp4',
+        },
+      ],
+    },
+    manifesto:
+      'Watson is led by strategy, influenced by design and inspired by technology',
+    description:
+      '<p>Great work stems from great processes. And before we ever sit down to design anything, we dive deep into the ask. We outline the challenges and objectives, figuring out the best ways to connect to our audience. We consider social behaviors, trends, and data to help inform our creative concepts. Then we think about how to connect all the pieces, to create a holistic campaign with the greatest possible reach. Once we have a clear vision of what’s ahead, we design and iterate, constantly problem-solving along the way, as we strive to create thought-provoking, dynamic, and relevant work.</p>',
+    partners: [
+      'A24',
+      'Amazon\u0020Studios',
+      'Fox\u0020Searchlight',
+      'FX',
+      'Focus\u0020Features',
+      'Lionsgate',
+      'Magnolia',
+      'Paramount',
+      'Riot\u0020Games',
+      'Sony\u0020Pictures',
+      'Universal',
+      'Walt\u0020Disney\u0020Pictures',
+      'Warner\u0020Bros',
+    ],
+    business: {
+      strategy: [
+        'Brand\u0020and\u0020Campaign\u0020Positioning',
+        'Social\u0020Strategy',
+        'Paid\u0020Social\u0020Consulting',
+        'Copywriting',
+        'Community\u0020Management',
+      ],
+      design: [
+        'Social\u0020Content',
+        'Site\u0020Design',
+        'Identity\u0020\u0026\u0020Branding',
+        'Posters\u0020\u0026\u0020Illustration',
+        'Video\u0020Production\u0020\u0026\u0020Editing',
+        'Motion\u0020Graphics\u0020\u0026\u0020Animation',
+        'User\u0020Experience',
+      ],
+      technology: [
+        'Interactive\u0020Development',
+        'Mobile\u0020Experience',
+        'AR\u0020\u0026\u0020VR',
+        'WebGL',
+        'Craft\u0020CMS',
+        'Native\u0020Apps',
+      ],
+    },
+    awards: [
+      {
+        contest: 'fwa',
+        src: 'cms/fwa.png',
+        count: 59,
+      },
+      {
+        contest: 'awwwards',
+        src: 'cms/awwwards.png',
+        count: 51,
+      },
+      {
+        contest: 'behance',
+        src: 'cms/behance.png',
+        count: 57,
+      },
+      {
+        contest: 'webby',
+        src: 'cms/webby.png',
+        count: 17,
+      },
+      {
+        contest: 'clio',
+        src: 'cms/clio.png',
+        count: 38,
+      },
+      {
+        contest: 'shorty',
+        src: 'cms/shorty.png',
+        count: 3,
+      },
+      {
+        contest: 'oneshow',
+        src: 'cms/oneshow.png',
+        count: 2,
+      },
+    ],
+    culture:
+      '<p>We’re always looking to work alongside collaborative and ambitious partners. We believe transparency and communication help build the best relationships and ultimately, the best work. If you have a project you’d like to discuss, we’d love to talk more.</p>',
+  },
+  contact: {
+    address: ['5900 Wilshire Blvd,', 'Ste. 2050'],
+    city: ['Los Angeles,', 'CA 90036, USA'],
+    telephone: '+1 323 465 9225',
+    location:
+      'https://www.google.com/maps/place/5900+Wilshire,+Los+Angeles,+CA+90036/@34.0636713,-118.3591073,19z/',
+    emails: ['Newbusiness', 'Contact', 'Jobs'],
+  },
+};
